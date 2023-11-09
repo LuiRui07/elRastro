61175b363e54d9efac0ccf439587a9ae87325697
Luis Ruiz <100932068+LuiRui07@users.noreply.github.com>
2023-11-09 11:01:07 +0100

    Update productoRoutes.js

--- back/Productos/routes/productoRoutes.js
+++ back/Productos/routes/productoRoutes.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId;
 const productosSchema = require("../models/productos.js");
 
+router.use(express.json());
+router.use(cors());
+
 // create 
 router.post("/", (req, res) => {
   const user = productosSchema(req.body);
@@ -19,6 +22,7 @@ router.get("/", (req, res) => {
     .find()
     .then((data) => res.json(data))
     .catch((error) => res.json({ message: error }));
+    res.send("Esta es la API");
 });
 
 // get 
@@ -65,20 +69,7 @@ router.put("/:id", (req, res) => {
     .catch((error) => res.json({ message: error }));
 });
 
-// get productos ofertados por un usuario
-router.get("/productos-ofertados/:usuarioId", (req, res) => {
-  const { usuarioId } = req.params;
-  productosSchema
-    .find({ vendedor: usuarioId })
-    .sort({ fecha: -1 }) //Ordena en en fecha descendente
-    .then((productosOfertados) => {
-      if (productosOfertados.length === 0) {
-        return res.json({ message: "El usuario no ha ofertado ningún producto." });
-      }
-      res.json(productosOfertados);
-    })
-    .catch((error) => res.json({ message: error }));
-});
+
 
 module.exports = router;
 
