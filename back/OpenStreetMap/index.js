2679e688ebe6a76dd6f4756fd8888f9f7e5986e9
Chema <josemariafcanton@uma.es>
2023-11-11 10:30:02 +0100

    OpenStreetMap diria que terminado'

--- back/OpenStreetMap/index.js
+++ back/OpenStreetMap/index.js
@@ -8,11 +8,11 @@ const app = express();
 const port = process.env.PORT;
 app.use(express.json());
 
-const productoRoutes = require("./routes/productoRoutes.js")
+const productoRoutes = require("./routes/mapRouter.js")
 app.use('/mapa', productoRoutes);
 
 
 app.get("/",(req,res) =>{
-  res.send("Esta es la API")}
+  res.send("Esta es la API de OpenStreetMap")}
 )
-app.listen(port, console.log("Servidor escuchando en el puerto ", port))
+app.listen(port, console.log("Servidor de las calls de Mapa escuchando en el puerto ", port))
--- back/OpenStreetMap/routes/productoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const axios = require("axios");
-router.use(express.json());
-
-module.exports = router;
-
-
