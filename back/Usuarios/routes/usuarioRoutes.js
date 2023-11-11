a987d66765940a1f20e223280b5da1e798d9528e
ChemaFdezUma <100839162+ChemaFdezUma@users.noreply.github.com>
2023-11-11 19:08:25 +0100

    Funcion rara

--- back/Usuarios/routes/usuarioRoutes.js
+++ back/Usuarios/routes/usuarioRoutes.js
@@ -42,9 +42,9 @@ router.delete("/:id", (req, res) => {
 // update, comprobado con Postman
 router.put("/:id", (req, res) => {
   const { id } = req.params;
-  const { nombreCompleto, direccion} = req.body;
+  const { nombreCompleto, direccion } = req.body;
   usuariosSchema
-    .updateOne({ _id: id }, { $set: { nombreCompleto, direccion} })
+    .updateOne({ _id: id }, { $set: { nombreCompleto, direccion } })
     .then((data) => res.json(data))
     .catch((error) => res.json({ message: error }));
 });
@@ -54,8 +54,7 @@ router.get("/nombre/:nombre", (req, res) => {
   const { nombre } = req.params;
   usuariosSchema
     .find({ nombreCompleto: { $regex: nombre, $options: "i" } })
-    .then((data) => 
-    {
+    .then((data) => {
       if (data.length === 0) {
         return res.json({ message: "No se ha encontrado ningún usuario con ese nombre." });
       }
@@ -85,68 +84,104 @@ router.get("/direccion/:direccion", (req, res) => {
 router.get("/compradores/:productoId", (req, res) => {
   const { productoId } = req.params;
   axios.get('http://localhost:5001/productos/' + productoId)
-  .then((response) => {
-    const {data} = response;
-    const {message} = data;
-    if(data.length === 0){
-      return res.json({message: 'No se ha encontrado ningún producto con ese id.'})
-    }else if(data.length > 1){
-      return res.json({message: 'Hay más de un producto con ese id.'})
-    }
-    var compradores = [];
-    var puja = null;
-    for(let i = 0; i < data.length; i++){
-      puja = data[i].pujaMasAlta;
-      axios.get('http://localhost:5000/pujas/' + puja._id)
-      .then((response) => {
-        const {data} = response;
-        const {message} = data;
-        compradores.push(data[0].comprador);
-      }) 
-    }
-    res.json(compradores);
-  })
+    .then((response) => {
+      const { data } = response;
+      const { message } = data;
+      if (data.length === 0) {
+        return res.json({ message: 'No se ha encontrado ningún producto con ese id.' })
+      } else if (data.length > 1) {
+        return res.json({ message: 'Hay más de un producto con ese id.' })
+      }
+      var compradores = [];
+      var puja = null;
+      for (let i = 0; i < data.length; i++) {
+        puja = data[i].pujaMasAlta;
+        axios.get('http://localhost:5000/pujas/' + puja._id)
+          .then((response) => {
+            const { data } = response;
+            const { message } = data;
+            compradores.push(data[0].comprador);
+          })
+      }
+      res.json(compradores);
+    })
 
 });
 
 
 router.get('/propietarioPorID/:productoId', (req, res) => {
-  const {productoId} = req.params;
+  const { productoId } = req.params;
   axios.get('http://localhost:5001/productos/' + productoId)
-  .then((response) => {
-    const {data} = response;
-    const {message} = data;
-    if(message){
-      return res.json({message: message})
-    }
-    if(data.length === 0){
-      return res.json({message: 'No se ha encontrado ningún producto con ese id.'})
-    }else if(data.length > 1){
-      return res.json({message: 'Hay más de un producto con ese id.'})
-    }
-    res.json(data[0].vendedor);
-  })
+    .then((response) => {
+      const { data } = response;
+      const { message } = data;
+      if (message) {
+        return res.json({ message: message })
+      }
+      if (data.length === 0) {
+        return res.json({ message: 'No se ha encontrado ningún producto con ese id.' })
+      } else if (data.length > 1) {
+        return res.json({ message: 'Hay más de un producto con ese id.' })
+      }
+      res.json(data[0].vendedor);
+    })
 })
-//get usuarios a x distancia de ti, ESPERAR A QUE SE HAGA EL DE DISTANCIA CON EL SERVICIO EXTERNO
+//get usuarios a x distancia de ti, ESPERAR A QUE SE HAGA EL DE DISTANCIA CON EL SERVICIO EXTERNO, VAMOS A REVISAR
+router.get("/distancia/:usuarioID/:distancia", (req, res) => {
+  const { distancia } = req.params;
+  const { usuarioID } = req.params;
+  axios.get('http://localhost:5002/usuarios/' + usuarioID)
+    .then((response) => {
+      usuariosSchema.find().then((todosLosUsuarios) => {
+        const datosUsuario = response.data;
+        const { calle, numero, codigoPostal, ciudad, provincia, pais } = datosUsuario;
+        const direccion = calle + " " + numero + ", " + codigoPostal + ", " + ciudad + ", " + provincia + ", " + pais;
+        axios.get('http://localhost:5004/mapa/direccionCoordenadas/' + direccion)
+          .then((coordenadas) => {
+            const latUsuario = coordenadas.data.lat;
+            const lonUsuario = coordenadas.data.lon;
+            const devolver = todosLosUsuarios.filter((usuario) => {
+              const { calle, numero, codigoPostal, ciudad, provincia, pais } = usuario;
+              const direccion = calle + " " + numero + ", " + codigoPostal + ", " + ciudad + ", " + provincia + ", " + pais;
+              axios.get('http://localhost:5004/mapa/direccionCoordenadas/' + direccion)
+                .then((coord) => {
+                  const latUsuario2 = coord.data.lat;
+                  const lonUsuario2 = coord.data.lon;
+                  axios.get(`https://router.project-osrm.org/route/v1/driving/${lonUsuario},${latUsuario};${lonUsuario2},${latUsuario2}?overview=false`)
+                    .then((respuesta) => {
+                      const { distance } = respuesta.data.routes[0];
+                      if (distance <= distancia) {
+                        return true;
+                      }
+                    })
+                    .catch((error) => console.log(error))
+                }
+                )
+            })
+          res.json(devolver);
+          })
+      })
 
+    });
+});
 //AUXILIARES-------------------------------------------------------------------------------
 //Get de propietario de producto con ID x, devuelve el producto entero y de ahi podemos pillar el vendedor, auxiliar
 router.get('/propietario/:productoId', (req, res) => {
-  const {productoId} = req.params;
+  const { productoId } = req.params;
   axios.get('http://localhost:5001/productos/' + productoId)
-  .then((response) => {
-    const {data} = response;
-    const {message} = data;
-    if(message){
-      return res.json({message: message})
-    }
-    if(data.length === 0){
-      return res.json({message: 'No se ha encontrado ningún producto con ese id.'})
-    }else if(data.length > 1){
-      return res.json({message: 'Hay más de un producto con ese id.'})
-    }
-    res.json(data);
-  })
+    .then((response) => {
+      const { data } = response;
+      const { message } = data;
+      if (message) {
+        return res.json({ message: message })
+      }
+      if (data.length === 0) {
+        return res.json({ message: 'No se ha encontrado ningún producto con ese id.' })
+      } else if (data.length > 1) {
+        return res.json({ message: 'Hay más de un producto con ese id.' })
+      }
+      res.json(data);
+    })
 })
 
 module.exports = router
