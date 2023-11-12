7b4d4c74f240fab532e6b2ce8995cc19803b0f61
ChemaFdezUma <100839162+ChemaFdezUma@users.noreply.github.com>
2023-11-12 15:29:16 +0100

    Create ejecucion.bat

---
+++ back/ejecucion.bat
@@ -0,0 +1,18 @@
+@echo off
+
+rem Define the repositories to work on
+set repositories=CarbonFootprint OpenStreetMap Productos Pujas Usuarios
+
+rem Loop through the repositories in parallel
+for %%r in (%repositories%) do (
+    rem Change to the repository directory
+    cd C:\Users\steam\Documents\GitHub\elRastro\back\%%r
+
+    rem Run npm install
+    npm install
+
+    rem Start nodemon app in a separate window
+    start cmd /c nodemon app
+)
+
+echo All processes have been started.
