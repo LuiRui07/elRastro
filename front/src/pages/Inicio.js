b4d82f7301541b885a87bafd8f61da1bd6d7e3c8
Luis Ruiz <100932068+LuiRui07@users.noreply.github.com>
2023-11-29 00:12:43 +0100

    mejorando

--- front/src/pages/Inicio.js
+++ front/src/pages/Inicio.js
@@ -4,7 +4,7 @@ import '../css/Inicio.css';
 import '../components/SearchBar';
 import SearchBar from '../components/SearchBar';
 import Logo from '../media/logo.jpeg';
-
+import UserImage from '../media/user.jpg';
 
 
 
@@ -15,8 +15,10 @@ function Inicio() {
   useEffect(() => {
     axios.get('http://localhost:5001/productos')
       .then(response => {
-        setData(response.data); 
-        console.log('Datos del backend:', response.data);
+        if (response.data !== null){
+          setData(response.data); 
+          console.log('Datos del backend:', response.data);
+        }
       })
       .catch(error => {
         console.error('Error al obtener datos del backend:', error);
@@ -25,25 +27,36 @@ function Inicio() {
 
   return (
     <div style={{ textAlign: 'center'}}>
-      <nav className="navbar">
-        <img src={Logo} style={{ width: '80px', height: '80px'}} ></img>
-        <a href="/">Inicio</a>
-        <a href="#categorias">Categorias</a>
-        <a href="#servicios">Servicios</a>
-        <a href="#contacto">Perfil</a>
+      <nav className="navbar" style={{alignItems: 'center'  }}>
+        <img src={Logo} style={{ width: '80px', height: '80px', borderRadius: '90px', marginRight: 'auto' }} alt="Logo" />
+        <a style={{ color: 'white' }}>Buzon</a>
+        <a style={{ color: 'white' }}>Perfil</a>
+        <a href='/'>
+          <img src={UserImage} style={{ width: '50px', height: '50px', borderRadius: '90px', marginRight: "1%" }} alt="User" />
+        </a>
       </nav>
-      <header>
+      <nav className='navbar'>
+        <a style={{color: "white"}}>Categorias </a>
+        <a style={{color: "white"}}>Coches </a>
+        <a style={{color: "white"}}>Motos </a>
+        <a style={{color: "white"}}>Moda </a>
+        <a style={{color: "white"}}>Tecnología </a>
+      </nav>
+ 
+      <header className="elRastro">
         <h1>elRastro</h1>
         <p>
           Bienvenido a elRastro
         </p>
+      </header>
         <SearchBar/>
         <ul>
-          {data && data.map((producto, index) => (
+           {data && data.map((producto, index) => (
             <li key= {index}>{producto.nombre}</li>
-          ))}
+           ))}
+
         </ul>
-      </header>
+     
 
     </div>
   );
