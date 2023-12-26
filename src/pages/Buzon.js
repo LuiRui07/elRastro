00656836fcd30d4ac20ca4bdde93806dd77caa05
Luis Ruiz <100932068+LuiRui07@users.noreply.github.com>
2023-12-26 15:16:13 +0100

    Update Buzon.js

--- src/pages/Buzon.js
+++ src/pages/Buzon.js
@@ -26,32 +26,38 @@ function Buzon() {
 
   return (
     <div style={{ textAlign: 'center' }}>
-    <Navbar />
-    <h1 class="display-2" style={{marginTop: "2%", marginBottom: "2%"}}>Mensajes</h1>
-    {mensajes.length > 0 ? (
-      mensajes.map((mensaje, index) => (
-        <div className="card" key={index} onClick={() => window.location.href = `/chat/${mensaje.productoId}/${mensaje.remitente}/${mensaje.destinatario}`}>
-          <div className="card-body">
-            <h5 className="card-title">{mensaje.texto}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">
-              {new Date(mensaje.fechaEnvio).toLocaleDateString("es-ES", {
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-                hour: "numeric",
-                minute: "numeric",
-                second: "numeric",
-              })}
-            </h6>
-            <p className="card-text">{mensaje.mensaje}</p>
+      <Navbar />
+      <h1 className="display-2" style={{ marginTop: "2%", marginBottom: "2%" }}>Mensajes</h1>
+      
+      {mensajes.length > 0 ? (
+        mensajes.map((grupo) => (
+          <div key={grupo._id}>
+            {grupo.mensajes.map((mensaje, index) => (
+              <div className="card" key={index} onClick={() => window.location.href = `/chat/${mensaje.productoId}/${mensaje.remitente}/${mensaje.destinatario}`}>
+                <div className="card-body">
+                  <h5 className="card-title">{mensaje.texto}</h5>
+                  <h6 className="card-subtitle mb-2 text-muted">
+                    {new Date(mensaje.fechaEnvio).toLocaleDateString("es-ES", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                      hour: "numeric",
+                      minute: "numeric",
+                      second: "numeric",
+                    })}
+                  </h6>
+                  <p className="card-text">{mensaje.mensaje}</p>
+                </div>
+              </div>
+            ))}
           </div>
-        </div>
-      ))
-    ) : (
-      <h1 className='mt-4 tituloElRastro'>Aún no has hablado con nadie. ¡Anímate!</h1>
-    )}
-  </div>
+        ))
+      ) : (
+        <h1 className='mt-4 tituloElRastro'>Aún no has hablado con nadie. ¡Anímate!</h1>
+      )}
+    </div>
   );
+  
 }
 
 export default Buzon;
