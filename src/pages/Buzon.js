66f4b4b3f3258b2b0b8c34168abaab9dee3628b2
Pablogp15 <pablogplatero15@gmail.com>
2023-12-26 20:14:10 +0100

    Update Buzon.js

--- src/pages/Buzon.js
+++ src/pages/Buzon.js
@@ -48,7 +48,7 @@ function Buzon() {
             console.log(productoActual);
           return (
           <div key={grupo._id}>
-            <div className="card mb-3" onClick={() => window.location.href = `/chat/${grupo._id}/${grupo.mensajes[0].remitente}/${grupo.mensajes[0].destinatario}`}>
+            <div className="card mb-3" onClick={() => window.location.href = `/chat/${grupo._id}/${grupo.mensajes[0].destinatario}/${grupo.mensajes[0].remitente}`}>
               <div className="card-body">
                 <h5 className="card-title"> {productoActual.nombre}</h5>
                 <p className="card-text"> {grupo.mensajes[0].texto}</p>
