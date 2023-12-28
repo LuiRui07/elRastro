7905a425784af23e0dfe31027ed02b94c5d40c0a
ChemaFdezUma <100839162+ChemaFdezUma@users.noreply.github.com>
2023-12-28 21:46:43 +0100

    Update index.js

--- Productos/index.js
+++ Productos/index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 5001;
 app.use(express.json());
 app.use(cors({
-  origin: ['https://el-rastro-nine.vercel.app','http://localhost:3000'],
+  origin: 'https://el-rastro-nine.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
