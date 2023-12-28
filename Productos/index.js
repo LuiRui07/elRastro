029fb7fc382f1a964d52e11613ca8782c8fc8e3c
Luis Ruiz <100932068+LuiRui07@users.noreply.github.com>
2023-12-28 13:56:58 +0100

    Update index.js

--- Productos/index.js
+++ Productos/index.js
@@ -6,7 +6,11 @@ const app = express();
 
 const port = 5001;
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://el-rastro-nine.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 
 const productoRoutes = require("./routes/productoRoutes.js")
 app.use('/productos', productoRoutes);
